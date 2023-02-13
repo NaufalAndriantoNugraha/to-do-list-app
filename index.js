c5554e1d430d91d856f8af982a0c36e8de248ae2
NaufalAndriantoNugraha <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2023-02-13 20:52:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const TASK_INPUT = document.getElementById('section-header-input').ariaValueText;
-const BUTTON = document.querySelector('#section-header-button');
-const INCOMPLETE_LIST_PARENT = document.querySelector('#section-incomplete-task-list-parent');
+const ADD_BUTTON = document.getElementById('section-header-button');
+const INPUT_TEXT = document.getElementById('section-header-input');
+const TODO_CONTAINER = document.getElementById('section-incomplete-task-list-parent');
+// let toDoContainerHoldData = [];
 
-BUTTON.addEventListener('click', () => {
-  alert(TASK_INPUT);
-  //   if (TASK_INPUT == '') {
-  //     alert('Oh Please');
-  //   }
+ADD_BUTTON.addEventListener('click', function createTask() {
+  let taskListChildren = document.createElement('li');
+  taskListChildren.className = 'section-incomplete-task-list-children';
 
-  //   let incompleteListChildren = document.createElement('li');
-  //   incompleteListChildren.appendChild(taskInputValue);
-  //   INCOMPLETE_LIST_PARENT.appendChild(incompleteListChildren);
+  let taskInput = document.createElement('input');
+  taskInput.type = 'checkbox';
+  taskInput.className = 'section-incomplete-task-list-children-checkbox';
+
+  let taskLabel = document.createElement('label');
+  taskLabel.className = 'section-incomplete-task-list-children-content';
+  taskLabel.innerText = INPUT_TEXT.value;
+
+  let taskButton = document.createElement('h3');
+  taskButton.className = 'section-incomplete-task-list-children-delete';
+  taskButton.innerText = 'Delete';
+
+  if (INPUT_TEXT.value === '') {
+    alert('Please, insert some value!');
+  } else {
+    TODO_CONTAINER.appendChild(taskListChildren);
+    taskListChildren.appendChild(taskInput);
+    taskListChildren.appendChild(taskLabel);
+    taskListChildren.appendChild(taskButton);
+    // toDoContainerHoldData.push(TODO_CONTAINER);
+    INPUT_TEXT.value = '';
+  }
+
+  taskButton.addEventListener('click', function deleteTask() {
+    TODO_CONTAINER.removeChild(taskListChildren);
+  });
 });
